1f6d6df3b62f1aff84efc61b494ee251dc4f888f
Diego <agudelodiego22@gmail.com>
2023-03-25 19:53:59 -0500

    Created product details page but, it will need more work

---
+++ src/components/Slider.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react"
+import Styles from "../styles/Slider.module.css"
+import {Asset} from "@chec/commerce.js/types/asset"
+import Image from "next/image"
+import { motion } from "framer-motion"
+
+
+interface Props {
+  images: Asset[] | undefined
+}
+
+export const Slider = ({images}:Props) => {
+  
+  const [selected,setSelected] = useState("")
+  const [i,setI] = useState(0)
+  const [subs,setSubs] = useState<Asset[]>([])
+
+  useEffect(()=>{
+    if(images){
+      setSelected(images[i].url)
+      setSubs(images)
+    }
+  },[images])
+
+  const selectImage = (id:string) => {
+    if(images){
+      let found = subs.findIndex((image)=>{
+        return image.id == id
+      })
+      setI(found)
+      setSelected(images[found].url)
+    }
+  }
+
+  return (
+    <div className={Styles.slider_container}>
+
+      <div className={Styles.slider_imgContainer}>
+        <Image width={500} height={500} src={selected} alt="Product image" className={Styles.slider_image}/>
+      </div>
+
+      <div className={Styles.siler_subImgContainer}>
+        {subs.map((asset)=>{
+          return(
+
+            <motion.button 
+              whileTap={{scale:.7}}
+              onClick={()=>selectImage(asset.id)}
+              className={Styles.slider_noneButton} 
+              key={asset.id}>
+
+              <Image width={50} height={50} src={asset.url} className={Styles.slider_subImg} alt="Product button" />
+
+            </motion.button>
+            
+          )
+        })}
+      </div>
+    </div>
+  )
+}
--- src/pages/products/[puid].tsx
+++ src/pages/products/[puid].tsx
@@ -1,29 +1,51 @@
 import { CartContext } from "@/context/cart/CartProvider"
 import { ProductsContext } from "@/context/products/ProductsProvider"
+import { commerce } from "@/lib/commerce"
 import { Product } from "@chec/commerce.js/types/product"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 import { useContext } from "react"
+import Styles from "../../styles/ProductDetail.module.css"
+import Layout from "@/components/Layout"
+import Head from "next/head"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Image from "next/image"
+import { Slider } from "@/components/Slider"
 
 
 const ProductDetails = ()=>{
 
   const router = useRouter()
   //const {cartState} = useContext(CartContext)
-  const {productsState} = useContext(ProductsContext)
+  //const {productsState} = useContext(ProductsContext)
   const [product,setProduct] = useState<Product | null | undefined>(null)
   
   useEffect(()=>{
-    let id = router.query.puid
-    let productFound = productsState.data.find((product)=>{
-      return product.id == id
-    })
-    setProduct(productFound)
-    console.log(productFound)
+    let id = router.query.puid as unknown as string
+    commerce.products.retrieve(id)
+      .then((item)=>setProduct(item))
   },[router])
 
   return(
-    <h4>Hello</h4>
+    <Layout>
+
+      <Head>
+        <title>{product?.name}</title>
+      </Head>
+
+      <div className={Styles.product_container}>
+
+        <div className={Styles.product_imgContainer}>
+          <Slider images={product?.assets}/>
+        </div>
+
+        <div className={Styles.product_detailsContainer}>
+          otro xd
+        </div>
+
+      </div>
+
+    </Layout>
   )
 }
 
